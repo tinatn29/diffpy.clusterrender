afc1b9ca26c084c17cedc34859f99f34b345e32a
tinatn29 <tanaporn.nanarong@gmail.com>
2026-01-23 17:57:43 -0500

    add DataFrame initialization option

--- src/diffpy/clusterrender/structuredf.py
+++ src/diffpy/clusterrender/structuredf.py
@@ -19,21 +19,31 @@ class StructureDF(pd.DataFrame):
 
     Parameters
     ----------
+    *args, **kwargs
+        Arguments passed to the pandas DataFrame constructor to initialize
+        the DataFrame.
+        These are ignored if `structure` or `filename` is provided.
     structure : pymatgen.core.Structure, optional
         A Structure object from pymatgen.core to initialize the DataFrame.
     filename : str, optional
         Path to a file to read the structure (or cluster) from.
         Accepted file formats include cif and xyz.
+    site_index : int, optional
+        Index of the central atom in the structure.
     """
 
-    def __init__(self, structure=None, site_index=None, filename=None):
+    def __init__(
+        self, *args, structure=None, site_index=None, filename=None, **kwargs
+    ):
         # load from Structure object if provided
         if isinstance(structure, Structure):
-            self._from_structure(structure, site_index)
+            self.from_structure(structure, site_index)
             return
         # load from file if a filename is provided (without a Structure)
         elif filename is not None:
-            self._from_file(filename, site_index)
+            self.from_file(filename, site_index)
             return
+        # or try initializing with generic DataFrame arguments
         else:
-            raise ValueError("Either structure or filename must be provided.")
+            super().__init__(*args, **kwargs)
+            return
