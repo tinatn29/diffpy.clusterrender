ba49fb59f133cab09ae920846988cba3334042ce
tinatn29 <tanaporn.nanarong@gmail.com>
2026-01-26 18:03:44 -0500

    edit Docstring

--- src/diffpy/clusterrender/structuredf.py
+++ src/diffpy/clusterrender/structuredf.py
@@ -17,19 +17,17 @@ class StructureDF(pd.DataFrame):
     coordination shells, specifying the central atom (0) and its
     neighboring atoms (1, 2, ...).
 
-    Parameters
+    Methods
+    -------
+    from_structure(structure, site_index=None)
+        Load structure data from a pymatgen Structure object.
+    from_file(filename, site_index=None)
+        Load structure data from a file.
+
+    Attributes
     ----------
-    *args, **kwargs
-        Arguments passed to the pandas DataFrame constructor to initialize
-        the DataFrame.
-        These are ignored if `structure` or `filename` is provided.
-    structure : pymatgen.core.Structure, optional
-        A Structure object from pymatgen.core to initialize the DataFrame.
-    filename : str, optional
-        Path to a file to read the structure (or cluster) from.
-        Accepted file formats include cif and xyz.
-    site_index : int, optional
-        Index of the central atom in the structure.
+    _constructor : property
+        Ensures that DataFrame operations return StructureDF objects.
     """
 
     @property
@@ -39,6 +37,25 @@ class StructureDF(pd.DataFrame):
     def __init__(
         self, *args, structure=None, site_index=None, filename=None, **kwargs
     ):
+        """Initialize StructureDF from a Structure object, a file, or
+        generic DataFrame arguments.
+
+        Parameters
+        ----------
+        structure : pymatgen.core.Structure, optional
+            The pymatgen Structure object to load the structure from.
+        site_index : int, optional
+            The index of atom in the structure to be treated as the
+            central atom.
+        filename : str, optional
+            The path to the structure file.
+            Accepted formats include CIF and XYZ.
+        *args, **kwargs
+            The arguments to initialize a pandas DataFrame with a
+            generic pandas constructor.
+            Accepted arguments are described in
+            https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html.
+        """
         # load from Structure object if provided
         if isinstance(structure, Structure):
             self.from_structure(structure, site_index)
